0ebb064d7123de86931fdc8fe74d760bc57d2b1a
Michał Wróblewski <272488@student.pwr.edu.pl>
2024-10-20 12:28:34 +0200

    add newBehaviour Script

---
+++ My project (1)/Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Deklaracja zmiennej na poziomie klasy
+    public Button sendScoreButton; // Przycisk do wysyłania wyniku
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Tutaj możesz umieścić kod, który ma być wykonany przy starcie gry
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Tutaj możesz umieścić kod, który ma być wykonany w każdej klatce
+    }
+}
